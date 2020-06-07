5442439b9d5bea265ce923e88a08fd5e71b10815
Ege Türker <66169371+EgeTurker@users.noreply.github.com>
2020-06-07 21:20:01 +0300

    Create ef.js

---
+++ ef.js
@@ -0,0 +1,105 @@
+var canvas = document.createElement("canvas");
+var context = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style = "position:absolute;top:0px;left:0px;background-color:black;";
+document.body.style = "overflow:hidden;background-color:black";
+document.body.appendChild(canvas);
+var charges = [];
+class PointCharge {
+  constructor(x, y, q, r) {
+    this.x = x;
+    this.y = y;
+    this.q = q;
+    this.r = r;
+    charges.push(this);
+    var lines=4;
+    if (q > 0) 
+    {
+      for (var i = 0; i < Math.abs(this.q) * lines; i++) {
+        var theta = Math.PI * 2 * (i+(q>0)/2) / (Math.abs(q) * lines);
+        new Tracer(x + Math.cos(theta) * r, y + Math.sin(theta) * r, q);
+      }
+    }
+  }
+  draw(drawingContext) {
+    drawingContext.beginPath();
+    drawingContext.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    drawingContext.fillStyle = `hsla(${240 * (this.q > 0)},${Math.floor(
+      100 * (1 - 1 / (Math.abs(this.q) / 16 + 1))
+    )}%,50%,1)`;
+    drawingContext.fill();
+  }
+}
+var tracers = [];
+class Tracer {
+  constructor(x, y, c) {
+    this.x = x;
+    this.y = y;
+    this.q = 1 - 2 * (c < 0);
+    tracers.push(this);
+    this.moving = true;
+  }
+  move(step, drawingContext) {
+    var vect = [0, 0];
+    for (var i = 0; i < charges.length; i++) {
+      var q = charges[i];
+      var rr = (this.x - q.x) ** 2 + (this.y - q.y) ** 2;
+      if (rr <= q.r ** 2 && q.q * this.q < 0) {
+        this.moving = false;
+        return;
+      }
+      var r = rr ** 0.5;
+      vect[0] += this.q * q.q / rr * (this.x - q.x) / r;
+      vect[1] += this.q * q.q / rr * (this.y - q.y) / r;
+    }
+    var mag = (vect[0] ** 2 + vect[1] ** 2) ** 0.5;
+    vect[0] /= mag;
+    vect[1] /= mag;
+    var oldX=this.x;
+    var oldY=this.y;
+    this.x += vect[0] * step;
+    this.y += vect[1] * step;
+    if(this.x>0&&
+       this.x<canvas.width&&
+       this.y>0&&
+       this.y<canvas.height)
+      {
+      drawingContext.beginPath();
+      drawingContext.lineWidth = 1;
+      drawingContext.lineCap = "round";
+      drawingContext.strokeStyle = "hsl(0,0%,100%)";
+      drawingContext.moveTo(this.x, this.y);
+      drawingContext.lineTo(this.x, this.y);
+      drawingContext.stroke();
+      }
+  }
+  static clear() {
+    tracers = [];
+  }
+  static drawAll(step, drawingContext) {
+    for (var i = 0; i < tracers.length; i++) {
+      if (tracers[i].moving) {
+        tracers[i].move(step, drawingContext);
+      }
+    }
+  }
+}
+for (var i = 0; i < canvas.width * canvas.height / 128**2; i++) {
+  var charge = 1 + Math.floor(Math.random() * 8);
+  for (var ii = -1; ii <= 1; ii += 2) {
+    var q = new PointCharge(
+      canvas.width * Math.random(),
+      canvas.height * Math.random(),
+      charge * ii,
+      4
+    );
+    q.draw(context);
+  }
+}
+setInterval(function() {
+	Tracer.drawAll(.25, context);
+	for (var i = 0; i < charges.length; i++) {
+		charges[i].draw(context);
+	}
+}, 1);
